438a40bcaa21eb45dec2b8b86357300dbfd6c8ab
tamaratosic98 <tosict98@gmail.com>
2021-02-12 14:38:44 +0100

    instalacija flight framework-a

---
+++ vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '51a9406bce66166dd660928bdbec9a7e5fdd3787',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '51a9406bce66166dd660928bdbec9a7e5fdd3787',
+    ),
+    'mikecao/flight' => 
+    array (
+      'pretty_version' => 'v1.3.8',
+      'version' => '1.3.8.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'b3120f8db2ae3b358c1c7a412a92d7aa008a57b4',
+    ),
+  ),
+);
